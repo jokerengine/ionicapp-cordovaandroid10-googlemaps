515a8c9c7961a2bb9053608d63b04fc82e138ea2
Victor Hargrave <victor.hargrave@edorex.ch>
2021-08-20 16:49:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ionicapp-cordovaandroid10-googlemaps
 Github Repo for testing a bug in the google maps plugin provided for cordova-android-10/ionic
+
+# installation
+To run the application you'll need the newest version of the ionic-cli, cordova-cli and android studio on your computer
+
+Clone this project from git and run the following commands: 
+```
+npm ci
+```
+
+```
+ionic cordova platform rm android
+ionic cordova platform add android@10.1.0
+```
+
+You can now plug in your android device to the computer and then run
+```
+ionic cordova run android --device
+```
+
+
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,57 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+import {Component, OnInit} from '@angular/core';
+import {Platform} from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  map: GoogleMap;
+  constructor(private platform: Platform) {}
+
+  ionViewDidEnter(): void {
+    debugger;
+    this.platform.ready().then(() => {
+      this.loadMap();
+    });
+  }
+
+  loadMap() {
+    const mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    const marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+}
